6ff3878cbd9d2b6570e88051b3ea6934a45694bb
danocmx <glencocomaster@centrum.cz>
2020-05-26 13:57:21 +0200

    Add new tests for new method

--- test/inventory.js
+++ test/inventory.js
@@ -3,10 +3,8 @@ const { assert } = require('chai');
 
 const Inventory = require('../src/Inventory');
 
-/**
- * @constant
- */
-const TEN_SECONDS_IN_MILISECONDS = 10 * 1000;
+const TEN_SECONDS = 10 * 1000;
+const THIRTY_SECONDS = TEN_SECONDS * 3;
 
 describe('Instance', () => {
 	it('create', () => {
@@ -22,7 +20,23 @@ describe('Instance', () => {
 });
 
 describe('New Method', () => {
-	it('request', () => {
+	it('Get first 500', () => {
+		return new Inventory()
+			.get({
+				count: null,
+				appID: 730,
+				contextID: 2,
+				steamID: '76561198122604075',
+			})
+			.then((results) => {
+				assert.isArray(results, 'Inventory is not an array.');
+				assert.isNotEmpty(results, 'No items received.');
+				assert.isBelow(results.length, 501, 'Received more items in one page request.');
+			})
+
+	})
+
+	it('One page inventory', () => {
 		const inventory = new Inventory({ steamID: '76561198144346135' });
 
 		return inventory
@@ -34,7 +48,37 @@ describe('New Method', () => {
 				assert.isArray(results, 'Inventory is not an array.');
 				assert.isNotEmpty(results, 'No items received.');
 			});
-	});
+	}).timeout(TEN_SECONDS)
+
+	it('3 page inventory', () => {
+		const inventory = new Inventory();
+
+		return inventory
+			.get({
+				appID: 753,
+				contextID: 6,
+				steamID: '76561198144346135',
+				count: 12500,
+			})
+			.then((results) => {
+				assert.isArray(results, 'Inventory is not an array.');
+				assert.isNotEmpty(results, 'No items received.');
+				assert.isAbove(results.length, 10001, 'Inventory has less than 10001 items. If SID\'s inventory is low omit this error.');
+			})
+	}).timeout(THIRTY_SECONDS)
+
+	it('All items', () => {
+		return new Inventory()
+			.get({
+				appID: 753,
+				contextID: 6,
+				steamID: '76561198144346135'
+			})
+			.then((results) => {
+				assert.isArray(results, 'Inventory is not an array.');
+				assert.isNotEmpty(results, 'No items received.');
+			})
+	}).timeout(THIRTY_SECONDS)
 });
 
 describe('Old method', () => {
@@ -53,6 +97,5 @@ describe('Old method', () => {
 				assert.isArray(results, 'Inventory is not an array.');
 				assert.isNotEmpty(results, 'No items received.');
 			});
-	})
-		.timeout(TEN_SECONDS_IN_MILISECONDS);
+	}).timeout(TEN_SECONDS);
 });
