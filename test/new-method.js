b1cc3733aa4a3a495afd1f5a46fc51ed1cd9a8f6
danocmx <glencocomaster@centrum.cz>
2020-05-27 18:04:49 +0200

    Add more tests and split them into multiple files

--- test/new-method.js
+++ test/new-method.js
@@ -1,23 +1,9 @@
-/* eslint-disable */
 const { assert } = require('chai');
 
 const Inventory = require('../src/Inventory');
 
 const TEN_SECONDS = 10 * 1000;
 const THIRTY_SECONDS = TEN_SECONDS * 3;
-
-describe('Instance', () => {
-	it('create', () => {
-		const inventory = new Inventory({
-			steamID: '76561198122604075',
-			headers: {
-				'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
-			},
-		});
-
-		assert.instanceOf(inventory, Inventory);
-	});
-});
 
 describe('New Method', () => {
 	it('Get first 500', () => {
@@ -79,23 +65,4 @@ describe('New Method', () => {
 				assert.isNotEmpty(results, 'No items received.');
 			})
 	}).timeout(THIRTY_SECONDS)
-});
-
-describe('Old method', () => {
-	it('request', () => {
-		const inventory = new Inventory({
-			method: 'old',
-		});
-
-		return inventory
-			.get({
-				steamID: '76561198259733876', // Paginated
-				appID: 440,
-				contextID: 2,
-			})
-			.then((results) => {
-				assert.isArray(results, 'Inventory is not an array.');
-				assert.isNotEmpty(results, 'No items received.');
-			});
-	}).timeout(TEN_SECONDS);
 });
